58f074ee991e817ecde79a2fff656cb20e8eea55
Zedai00 <33319711+Zedai00@users.noreply.github.com>
2025-03-08 23:01:11 +0530

    Add read toggle prototype function

--- script.js
+++ script.js
@@ -19,30 +19,49 @@ function displayBooks() {
   myLibrary.forEach(book => {
     let title = document.createElement("div")
     title.innerText = `Title: ${book.title}`
+
     let author = document.createElement(
       "div")
     author.innerText = `Author: ${book.author}`
+
     let pages = document.createElement("div")
     pages.innerText = `Pages: ${book.pages}`
+
+    let src = (book.read) ? "./assets/checkbox.svg" : "./assets/cross.svg"
+    let img = document.createElement("img")
+    img.src = src
+    img.classList.add("imgReadBtn")
+    img.dataset.id = book.id
+    let read = document.createElement("div")
+    read.classList.add("read")
+    read.dataset.id = book.id
+    read.append("Read: ")
+    read.append(img)
+
     let info = document.createElement("div")
     info.classList.add("info")
+    info.append(title, author, pages, read)
 
-    let readBtn = document.createElement("button")
-    readBtn.clas
+    let rmdiv = document.createElement("div")
+    rmdiv.classList.add("rmdiv")
+    let remove = document.createElement("button")
+    remove.classList.add("remove")
+    remove.dataset.id = book.id
+    remove.innerText = "❌"
+    rmdiv.append(remove)
+
     let b = document.createElement("div")
     b.classList.add("book");
-    b.appendChild(title)
-    b.appendChild(author)
-    b.appendChild(pages)
+    b.append(info, rmdiv)
     library.appendChild(b)
   })
 }
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 20; i++) {
   addBookToLibrary("abc", "def", 12, true)
 }
 
-// displayBooks()
+displayBooks()
 
 const showButton = document.querySelector("#showButton")
 const bookDialog = document.querySelector("#bookDialog")
@@ -71,3 +90,30 @@ bookForm.addEventListener("submit", (e) => {
   displayBooks()
   bookDialog.close()
 })
+
+
+let library = document.querySelector(".library")
+library.addEventListener("click", (e) => {
+  console.log(e)
+  if (e.target.classList.contains("remove")) {
+    let id = e.target.dataset.id
+    const index = myLibrary.findIndex(book => book.id === id)
+    if (index !== -1) {
+      myLibrary.splice(index, 1)
+    }
+    displayBooks()
+  } else if (e.target.classList.contains("read") || e.target.classList.contains("imgReadBtn")) {
+    let id = e.target.dataset.id
+    const index = myLibrary.findIndex(book => book.id === id)
+    if (index !== -1) {
+      myLibrary[index].toggleReadStatus()
+    }
+    displayBooks()
+  }
+})
+
+Book.prototype.toggleReadStatus = function () {
+  this.read ? this.read = false : this.read = true
+}
+
+
